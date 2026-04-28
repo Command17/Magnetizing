9d2e0b44d04edcc7823e245d1ca318258c3ef9d2
Command17 <84639718+Command17@users.noreply.github.com>
2026-04-28 10:25:35 +0200

    Fix Sable support

--- common/src/main/java/com/github/command17/magnetizing/common/block/entity/MagneticBlockEntity.java
+++ common/src/main/java/com/github/command17/magnetizing/common/block/entity/MagneticBlockEntity.java
@@ -1,5 +1,6 @@
 package com.github.command17.magnetizing.common.block.entity;
 
+import com.github.command17.magnetizing.Magnetizing;
 import com.github.command17.magnetizing.common.block.MagneticBlock;
 import com.github.command17.magnetizing.common.item.component.ModItemComponents;
 import com.github.command17.magnetizing.common.util.MagneticPole;
@@ -20,10 +21,14 @@ import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
 import java.lang.reflect.Field;
-import java.util.Iterator;
+import java.lang.reflect.Method;
 import java.util.List;
 
 public class MagneticBlockEntity extends BlockEntity {
+    private static Object sableCompanionObj = null;
+    private static Method sableHelperMethod = null;
+    private static boolean checkedSable = false;
+
     public MagneticBlockEntity(BlockEntityType<? extends MagneticBlockEntity> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
     }
@@ -49,26 +54,40 @@ public class MagneticBlockEntity extends BlockEntity {
     // I can't include SableCompanion since it needs loom 1.15. Architectury loom is only available until 1.14
     private static Vec3 sableCompanionWorkAround(Level level, Vec3 pos) {
         try {
-            Class<?> sableCompanion = Class.forName("dev.ryanhcode.sable.companion.SableCompanion");
-            Field instance = sableCompanion.getField("INSTANCE");
-            return (Vec3) ((Class<?>) instance.get(MagneticBlockEntity.class)).getMethod("projectOutOfSubLevel", Level.class, Position.class)
-                    .invoke(MagneticBlockEntity.class, level, new Position() {
-                        @Override
-                        public double x() {
-                            return pos.x();
-                        }
+            if (checkedSable) {
+                return pos;
+            }
 
-                        @Override
-                        public double y() {
-                            return pos.y();
-                        }
+            if (sableHelperMethod == null) {
+                Class<?> sableCompanion = Class.forName("dev.ryanhcode.sable.companion.SableCompanion");
+                Magnetizing.LOGGER.info("Found Sable Companion");
+                Field instance = sableCompanion.getField("INSTANCE");
+                sableCompanionObj = instance.get(MagneticBlockEntity.class);
+                Magnetizing.LOGGER.info("Found Sable Companion's INSTANCE");
+                sableHelperMethod = sableCompanionObj.getClass().getMethod("projectOutOfSubLevel", Level.class, Position.class);
+                Magnetizing.LOGGER.info("Found Sable Companion's helper method");
+            }
 
-                        @Override
-                        public double z() {
-                            return pos.z();
-                        }
-                    });
-        } catch (Exception ignored) {}
+            return (Vec3) sableHelperMethod.invoke(sableCompanionObj, level, new Position() {
+                @Override
+                public double x() {
+                    return pos.x();
+                }
+
+                @Override
+                public double y() {
+                    return pos.y();
+                }
+
+                @Override
+                public double z() {
+                    return pos.z();
+                }
+            });
+        } catch (Exception e) {
+            Magnetizing.LOGGER.error("Did not find Sable Companion! Exception:", e);
+            checkedSable = true;
+        }
 
         return pos;
     }
